a1dbb1b451cffd6547a5fc7e135cbea8e05cc6ad
Ryan France <ryan@parall.ax>
2021-03-22 12:50:20 +0000

    Hooking up the reducer with dispatch calls to change state

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,12 +9,15 @@ const reducer = (state, action) => {
   // action = { colorToChange: string 'red'|'green'|'blue', amount: COLOR_INCREMENT }
 
   switch (action.colorToChange) {
-    case: 'red':
+    case 'red':
       return { ...state, red: state.red + action.amount };
-    case: 'green':
+      break;
+    case 'green':
       return { ...state, green: state.green + action.amount };
-    case: 'blue':
+      break;
+    case 'blue':
       return { ...state, blue: state.blue + action.amount };
+      break;
     default:
       return state;
   } 
@@ -22,12 +25,13 @@ const reducer = (state, action) => {
 
 const SquareScreen = () => {
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
+  const { red, green, blue } = state; // Destructure these vars from state
 
   return (
     <View>
-      <ColorCounter onIncrease={() => } onDecrease={() => } color="Red" />
-      <ColorCounter onIncrease={() => } onDecrease={() => } color="Blue" />
-      <ColorCounter onIncrease={() => } onDecrease={() => } color="Green" />
+      <ColorCounter onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT }) } onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT }) } color="Red" />
+      <ColorCounter onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })} onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })} color="Green" />
+      <ColorCounter onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })} onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })} color="Blue" />
 
       <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
     </View>
