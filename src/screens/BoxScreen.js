197bb7d27c3336e0e26c80e9bb5aa40c09ee6571
Ryan France <ryan@parall.ax>
2021-03-27 22:14:19 +0000

    Playing around with screen layout

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,47 @@ import {Text, View, StyleSheet} from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle1}>Child</Text>
+      <Text style={styles.textStyle2}>Child</Text>
+      <Text style={styles.textStyle3}>Child</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textStyle1: {
+    borderWidth: 1,
+    borderColor: 'red',
+    // flex: 1,
+    height: 100,
+    width: 100
+    // marginVertical: 20,
+    // marginHorizontal: 20
+  },
+  textStyle2: {
+    borderWidth: 1,
+    borderColor: 'red',
+    // flex: 1,
+    height: 100,
+    width: 100,
+    top: 100
+    // marginVertical: 20,
+    // marginHorizontal: 20
+  },
+  textStyle3: {
+    borderWidth: 1,
+    borderColor: 'red',
+    // flex: 1,
+    height: 100,
+    width: 100
+    // marginVertical: 20,
+    // marginHorizontal: 20
+  }
+});
 
 export default BoxScreen;
